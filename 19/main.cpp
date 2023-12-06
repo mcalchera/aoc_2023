629a9d844e21f500c3d5afd3dbb677dc5cd728ff
Mark Calchera <mcalchera@gmail.com>
2023-12-05 20:12:32 -0600

    add timing to template

--- 19/main.cpp
+++ 19/main.cpp
@@ -10,7 +10,7 @@ int main (int argc, char ** argv) {
   auto start = chrono::high_resolution_clock::now();
 
   auto end = chrono::high_resolution_clock::now();
-  auto duration = chrono::duration_cast<chrono::microseconds(end - start);
-  cout << "Clock time: " << duration.count << " us" << endl;
+  auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
+  cout << "Clock time: " << duration.count() << " us" << endl;
   return 0;
 }
