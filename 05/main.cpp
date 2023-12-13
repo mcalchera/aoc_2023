8f86e76607fba71a873a2e42c3870b3a294e5467
Mark Calchera <mcalchera@gmail.com>
2023-12-13 14:34:13 -0600

    part 1 complete

--- 05/main.cpp
+++ 05/main.cpp
@@ -5,14 +5,27 @@
 #include <map>
 #include <string>
 #include <sstream>
+#include <algorithm>
 #include "../include/args.h"
 
 using namespace std;
 
 struct Mapping {
-  int dest_range;
-  int source_range;
-  int size;
+  long dest_range;
+  long src_range;
+  long size;
+
+  bool is_in_dest(long x) {
+    return (x >= dest_range) && (x <= dest_range + size);
+  }
+
+  bool is_in_source(long x) {
+    return (x >= src_range) && (x <= src_range + size);
+  }
+
+  long do_map(long x) {
+    return dest_range + (x - src_range);
+  }
 };
 
 struct Map {
@@ -21,21 +34,49 @@ struct Map {
   vector<Mapping> mappings;
 };
 
-vector<int> extract(string line) {
+struct Seed {
+  long start;
+  long size;
+};
+
+vector<long> extract(string line) {
   istringstream ss(line);
-  vector<int> ret;
+  vector<long> ret;
   string data;
   while (ss >> data) {
-    ret.push_back(stoi(data));
+    ret.push_back(stol(data));
   }
 
   return ret;
 }
 
+
+long part1(vector<Map> maps, long num) {
+  string types[7] = { "soil", "fertilizer", "water", "light", "temperature", "humidity", "location" };
+  string from = "seed";
+  long key = num;
+  for (long i = 0; i < 7; ++i) {
+    string to = types[i];
+    Map typemap = *find_if(maps.begin(), maps.end(), [to](Map map) { return map.map_to == to; });
+  
+    for (auto mapping : typemap.mappings) {
+      if (mapping.is_in_source(key)) {
+        key = mapping.do_map(key);
+        break;        
+      }
+    }
+
+  }
+  
+  return key;
+}
+
 int main (int argc, char ** argv) {
   process_args(argc, argv);
   auto start = chrono::high_resolution_clock::now();
-  vector<int> seeds;
+  vector<long> seeds;
+  vector<Seed> seeds2;
+  vector<long> locations;
   vector<Map> maps;
 
   ifstream in(argv[1]);
@@ -44,22 +85,22 @@ int main (int argc, char ** argv) {
     istringstream ss;
     string line;
     string data;
-    // ingest seeds
-    while (getline(in,line)) {
-      if (line.find("seeds:") != string::npos) {
-        ss.str(line);
-        ss >> data; //discard "seeds:"
-        while (ss >> data) {
-          seeds.push_back(stoi(data));
-        }
-        getline(in,line);
-      }
 
+    // first ingest seeds
+    getline(in,line);
+    if (line.find("seeds:") != string::npos) {
+      ss.str(line);
+      ss >> data; //discard "seeds:"
+      while (ss >> data) {
+        seeds.push_back(stol(data));
+      }
+    }
+    while (getline(in,line)) {
       if (line.empty()) { // New mapping section
         getline(in,line);
         Map map;
-        int start = 0;
-        int end = line.find('-');
+        long start = 0;
+        long end = line.find('-');
         map.map_from = line.substr(start, end-start);
         start = end + 4; //skip '-to-'
         end = line.find(' ', start);
@@ -68,15 +109,21 @@ int main (int argc, char ** argv) {
         getline(in, line);
       }
       // Next lines until an empty line will all be numbers.
-      vector<int> nums  = extract(line);
+      vector<long> nums  = extract(line);
       Mapping mapping = { nums[0], nums[1], nums[2] };
       maps.back().mappings.push_back(mapping);
         
     }
      
   }
-
   in.close();
+
+  for (auto seed : seeds) {
+    // follow maps and get location
+    locations.push_back(part1(maps, seed));
+  }
+
+  cout << "Part 1: " << *min_element(locations.begin(), locations.end()) << endl;
   auto end = chrono::high_resolution_clock::now();
   auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
   cout << "Clock time: " << duration.count() << " ms" << endl;
