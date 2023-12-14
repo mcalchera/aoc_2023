4225aecdc332a550894cc0fd74aae0fe76f3250f
Mark Calchera <mcalchera@gmail.com>
2023-12-14 09:18:29 -0600

    add inputs, process input code

--- 06/main.cpp
+++ 06/main.cpp
@@ -1,13 +1,49 @@
 #include <iostream>
 #include <fstream>
 #include <chrono>
+#include <string>
+#include <sstream>
+#include <vector>
 #include "../include/args.h"
 
 using namespace std;
 
+struct Race {
+  int time;
+  int dist;
+};
+
+vector<int> process_line(string line) {
+  istringstream ss(line);
+  vector<int> vals;
+  string data;
+  ss >> data; // get rid of header
+  while (ss >> data) {
+    vals.push_back(stoi(data));
+  }
+
+  return vals;
+}
+
 int main (int argc, char ** argv) {
   process_args(argc, argv);
   auto start = chrono::high_resolution_clock::now();
+  ifstream in(argv[1]);
+  vector<Race> races;
+
+  if (in.good()) {
+    string str;
+    getline(in, str);
+    vector<int> times = process_line(str);
+    getline(in, str);
+    vector<int> dists = process_line(str);
+
+    for (int i = 0; i < times.size(); ++i) {
+      Race r = { times[i], dists[i] };
+      races.push_back(r);
+    }
+  }
+
 
   auto end = chrono::high_resolution_clock::now();
   auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
