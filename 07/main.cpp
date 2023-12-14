cc24e05a1aa9de689259c5faf4695e0160ba80c9
Mark Calchera <mcalchera@gmail.com>
2023-12-14 14:24:45 -0600

    scrubbing inputs and adding to gitignore

--- 07/main.cpp
+++ 07/main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 #include <fstream>
 #include <chrono>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <string>
+#include <sstream>
 #include "../include/args.h"
 
 using namespace std;
+
+map<char,int> face_values = {
+  { 'A', 14 },
+  { 'K', 13 },
+  { 'Q', 12 },
+  { 'J', 11 },
+  { 'T', 10 }
+};
 
 int main (int argc, char ** argv) {
   process_args(argc, argv);
