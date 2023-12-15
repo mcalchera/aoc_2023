5e9124fe9f5e60eb66790add40265786cffdc764
Mark Calchera <mcalchera@gmail.com>
2023-12-15 15:06:18 -0600

    determine hand type

--- 07/main.cpp
+++ 07/main.cpp
@@ -22,6 +22,53 @@ map<char,int> face_values = {
 struct Hand {
   string hand;
   int bid;
+  int type;
+
+  // hands are ranked
+  // 5 of a kind = 6
+  // 4 of a kind = 5
+  // full house = 4
+  // 3 of a kind = 3
+  // 2 pair = 2
+  // 2 of a kind = 1
+  // high card = 0
+  void set_hand_type() {
+    string cards = hand;
+    map<char,int> ch_count;
+    vector<int> counts;
+
+    // count how many time each character shows up
+    for (auto ch : cards) {
+      ++ch_count[ch];
+    }
+    for (auto it = ch_count.begin(); it != ch_count.end(); ++it) {
+      counts.push_back(it->second);
+    }
+    //reverse sort counts
+    sort(counts.rbegin(),counts.rend());
+   
+    if (counts[0] == 5) {
+      type = 6;
+    }
+    else if (counts[0] == 4) {
+      type = 5;
+    }
+    else if (counts[0] == 3 && counts[1] == 2) {
+      type = 4;
+    }
+    else if (counts[0] == 3) {
+      type = 3;
+    }
+    else if (counts[0] == counts[1]) {
+      type = 2;
+    }
+    else if (counts[0] == 2) {
+      type = 1;
+    }
+    else {
+      type = 0;
+    }
+  }
 };
 
 Hand process_line(string line) {
@@ -29,14 +76,10 @@ Hand process_line(string line) {
   string h,b;
   ss >> h >> b;
   Hand hand = { h, stoi(b) }; 
-
+  hand.set_hand_type();
   return hand;
 }
 
-int hand_type (Hand hand) {
-
-    return 0;
-}
 
 int main (int argc, char ** argv) {
   process_args(argc, argv);
